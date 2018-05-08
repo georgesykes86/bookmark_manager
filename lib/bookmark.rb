57dd2a07a3298a9166b86e29bb043b24df322900
George Sykes <georgesykes86@gmail.com>
2018-05-08 14:30:29 +0100

    Adds bookmark.rb

---
+++ lib/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark
+
+  def self.all
+    ['http://bbc.co.uk', 'http://makersacademy.com', 'http://google.com']
+  end
+end
--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -1,6 +1,6 @@
 feature 'it loads up initial page' do
   scenario 'it show Bookmak Manager' do
     visit('/')
-    expect(page).to have_content('Bookmak Manager')
+    expect(page).to have_content('Bookmark Manager')
   end
 end
