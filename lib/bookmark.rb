6334c12f60a266af06de19f6222405e03684eadf
Revilo1882 <oliverrharris@gmail.com>
2018-05-08 16:23:02 +0100

    remove user specification

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,7 +2,7 @@
 class Bookmark
 
   def self.all
-    con = PG.connect :dbname => 'bookmark_manager', :user => ENV['USER']
+    con = PG.connect :dbname => 'bookmark_manager'
     rs = con.exec "SELECT url FROM bookmarks"
     rs.values
   end
